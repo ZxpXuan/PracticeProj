24a3cc7f9024f1f4664475d5a28ffd136b1219d3
ZxpXuan <38170958+ZxpXuan@users.noreply.github.com>
2018-04-11 23:51:36 +0800

    Up

---
+++ ROLL-BALL/New Unity Project/Assets/scripts/cameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+    public GameObject PPlayer;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {     
+        offset = transform.position- PPlayer.transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        transform.position = PPlayer.transform.position + offset;
+
+    }
+}
