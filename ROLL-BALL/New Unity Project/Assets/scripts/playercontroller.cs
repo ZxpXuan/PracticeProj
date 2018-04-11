ca5455d4396ccd1c771e444cc12735efe52e78c6
ZxpXuan <38170958+ZxpXuan@users.noreply.github.com>
2018-04-11 23:23:13 +0800

    BALL1

---
+++ ROLL-BALL/New Unity Project/Assets/scripts/playercontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizental = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizental, 0, moveVertical);
+        rb.AddForce(movement*speed);
+
+    }
+
+}
